32e492aae7087dee7a47fa2ab058d2e09a5dedbc
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2013-09-19 22:53:33 +0800

    Add clean.sh to clean up generated files

---
+++ clean.sh
@@ -0,0 +1,2 @@
+rm *.mli *.cmo *.cmi
+rm jlite_parser.ml jlite_lexer.ml as1 jlite_parser.output
--- make.sh
+++ make.sh
@@ -1,8 +1,7 @@
 export OCAMLRUNPARAM=''
 # export OCAMLRUNPARAM=''
 
-rm *.mli *.cmo *.cmi
-rm jlite_parser.ml jlite_lexer.ml as1 jlite_parser.output
+./clean.sh
 
 ocamlc -c jlite_structs.ml
 ocamlc -c displayfunc.ml
